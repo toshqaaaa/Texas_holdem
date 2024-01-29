fa7a7a6102132c0d9dac2bb24422e75fbcbb6f27
toshqaaaa <Lebe9@mail.ru>
2024-01-29 19:32:29 +0300

    utility class change new methods added

--- src/main/java/utils/CardHelper.java
+++ src/main/java/utils/CardHelper.java
@@ -1,11 +1,10 @@
 package utils;
 
 import domain.HighGradeCard;
+import domain.Suit;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * CardHelper class is the utility class that provides some useful methods to analyze player's hand
@@ -16,33 +15,102 @@ public class CardHelper {
     }
 
     /**
-     * Verifies that all cards belong to the same suit
+     * Verifies that card value can be parsed
      *
-     * @param cards - player's hand
+     * @param cardValue - separate card
      */
-    public static boolean isSameSuit(Map<String, List<String>> cards) {
-        return cards.values().stream().mapToInt(Collection::size).sum() == 1;
+    public static boolean isCardValueValid(String cardValue) {
+        if (!HighGradeCard.DEFAULT.equals(HighGradeCard.getByCardValue(cardValue))) {
+            return true;
+        }
+        try {
+            int cardValueAsInt = Integer.parseInt(cardValue);
+            return cardValueAsInt >= 2 && cardValueAsInt <= 14;
+        } catch (NumberFormatException numberFormatException) {
+            System.err.println("Not valid card value: " + cardValue);
+            return false;
+        }
     }
 
     /**
-     * Get costs array of player's hand
+     * Verifies suit (second char at each card)
      *
-     * @param faceValues - face value (ru: номинал, достоинство карты. Перевод указан для уточнения двоякости понятия
-     *                   номинала/достоинства карты в английском языке).
+     * @param suit - string representation of card suit
      */
-    public static Integer[] getAscendingCostsListByFaceValues(Set<String> faceValues) {
-        return faceValues.stream().map(faceValue -> {
-                    HighGradeCard highGradeCard = HighGradeCard.getById(faceValue);
-                    if (HighGradeCard.DEFAULT.equals(highGradeCard)) {
-                        return Integer.parseInt(faceValue);
-                    }
-                    return highGradeCard.getCost();
-                })
-                .sorted().toArray(Integer[]::new);
+    public static boolean isSuitValid(String suit) {
+        return !Suit.DEFAULT.equals(Suit.getBySuitValue(suit));
     }
 
     /**
-     * Verifies that cards order is sequential
+     * Verifies face value and suit of the card
+     *
+     * @param card - separate card
+     */
+    private static boolean isCardValid(String card) {
+        boolean isLengthValid = card.length() == 2;
+        String cardValue = card.substring(0, card.length() - 1);
+        String suit = card.substring(card.length() - 1);
+        boolean cardValueValid = isCardValueValid(cardValue);
+        boolean suitValid = isSuitValid(suit);
+        return isLengthValid && cardValueValid && suitValid;
+    }
+
+    /**
+     * Verifies all cards before PokerHand is instantiated
+     *
+     * @param hand - String representation of poker hand
+     */
+    public static boolean isHandValid(String hand) {
+        String[] separateCards = hand.split(" ");
+        if (separateCards.length != 5) {
+            return false;
+        }
+
+        return Arrays.stream(separateCards).allMatch(CardHelper::isCardValid);
+    }
+
+    /**
+     * Parse input string to Map<Integer, List<String>> where
+     * key - is card value;
+     * value - collection of suits
+     *
+     * @param cards - validated cards as string
+     */
+    public static Map<Integer, List<String>> handParser(String cards) {
+        String[] cardsArray = cards.split(" ");
+        Map<Integer, List<String>> cardsMap = Arrays.stream(cardsArray)
+                .collect(Collectors.groupingBy(CardHelper::cardValueParser,
+                        Collectors.mapping(card -> card.substring(card.length() - 1), Collectors.toList())));
+        return new TreeMap<>(cardsMap);
+    }
+
+    /**
+     * Parse input card by value (first char at string)
+     *
+     * @param card - a card from hand
+     */
+    private static Integer cardValueParser(String card) {
+        String cardValue = card.substring(0, card.length() - 1);
+
+        HighGradeCard highGradeCard = HighGradeCard.getByCardValue(cardValue);
+        return HighGradeCard.DEFAULT.equals(highGradeCard) ? Integer.parseInt(cardValue)
+                : highGradeCard.getCost();
+    }
+
+    /**
+     * Verify that all suits from hand are the same
+     *
+     * @param suits - suits collection
+     */
+
+    public static boolean isSameSuit(Collection<List<String>> suits) {
+        return suits.stream().flatMap(Collection::stream).collect(Collectors.toSet()).size() == 1;
+    }
+
+    /**
+     * Verifies that cards order is sequential. This method is only used in case when there are 5 different keys
+     * into card map (collection of faceValues is already ordered that is why difference between last and first element,
+     * in case of sequential order, will be fixed and equals to 4)
      * Example 1:
      * input: 2, 3, 4, 5, 6
      * output: true
@@ -53,8 +121,8 @@ public class CardHelper {
      * @param faceValues - face value (ru: номинал, достоинство карты. Перевод указан для уточнения двоякости понятия
      *                   номинала/достоинства карты в английском языке)
      */
-    public static boolean isInSequentialOrder(Set<String> faceValues) {
-        Integer[] costs = getAscendingCostsListByFaceValues(faceValues);
+    public static boolean isInSequentialOrder(Set<Integer> faceValues) {
+        Integer[] costs = faceValues.toArray(Integer[]::new);
         return costs[costs.length - 1] - costs[0] == 4;
     }
 }
