1d6c8ec77d13010c66f9412adb4463161f7a3cfc
toshqaaaa <Lebe9@mail.ru>
2024-01-29 23:51:31 +0300

    add initial data from task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,18 @@
+import org.example.domain.poker.PokerHand;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<PokerHand> hands = new ArrayList<>();
+        hands.add(new PokerHand("KS 2H 5C JD TD"));
+        hands.add(new PokerHand("2C 3C AC 4C 5C"));
+
+        System.out.println("Initial order: " + hands);
+
+        Collections.sort(hands);
+
+        System.out.println("Result order " + hands);
     }
 }
